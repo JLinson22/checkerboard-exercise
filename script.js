42ce08bdb5514c65e313218e6bad60f9a0def302
Jacob Linson <jlinson16@gmail.com>
2022-12-14 22:23:26 -0600

    checkerboard-complete

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Your JS goes here
+function redSquare() {
+    let div = document.createElement('div')
+    div.style.height = '11.1%'
+    div.style.width = '11.1%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '11.1%'
+    div.style.backgroundColor = 'red'
+    body.appendChild(div)
+    
+}
+
+function blackSquare() {
+    let div = document.createElement('div')
+    div.style.height = '11.1%'
+    div.style.width = '11.1%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '11.1%'
+    div.style.backgroundColor = 'black'
+    body.appendChild(div)
+    
+}
+var body = document.getElementsByTagName('body')[0]
+body.style.display = 'flex'
+body.style.flexWrap = 'wrap'
+for (var i = 0; i < 50; i++) {
+    redSquare()
+    blackSquare()
+}
